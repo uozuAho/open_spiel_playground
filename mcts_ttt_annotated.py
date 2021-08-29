943a9b5272b69deae151f292e0c8f8afaeca5a6e
Warwick Stone <warwick_stone@fastmail.com>
2021-08-29 15:36:09 +1000

    add mcts_step_verbose

--- mcts_ttt_annotated.py
+++ mcts_ttt_annotated.py
@@ -3,6 +3,7 @@
 # show how MCTS works.
 
 import math
+import time
 import numpy as np
 
 import pyspiel
@@ -28,7 +29,8 @@ def main():
     current_player_idx = state.current_player()
     current_player = players[current_player_idx]
     if current_player_idx == 0:
-      action = mcts_step_standard(current_player, state)
+      # action = mcts_step_standard(current_player, state)
+      action = mcts_step_verbose(current_player, state)
     else:
       action = current_player.step(state)
     action_str = state.action_to_string(current_player_idx, action)
@@ -49,10 +51,35 @@ def new_mcts_bot(game, rng=np.random.RandomState()):
       solve=True,
       random_state=rng,
       evaluator=mcts.RandomRolloutEvaluator(n_rollouts=20, random_state=rng),
-      verbose=True)
+      verbose=False)
 
 def mcts_step_standard(bot, state):
+  # does whatever the mcts bot.step does
   return bot.step(state)
+
+def mcts_step_verbose(bot, state):
+  # additional verbosity of what's going on within the MCTS algorithm
+  # note: set Verbose=False when instantiating the mcts bot
+  t1 = time.time()
+  root = bot.mcts_search(state)
+
+  best = root.best_child()
+
+  seconds = time.time() - t1
+  print(f"Finished {root.explore_count} sims in {seconds:.3f} secs, " +
+        f"{root.explore_count / seconds:.1f} sims/s")
+  print("Root:")
+  print(root.to_str(state))
+  print("Children:")
+  print(root.children_str(state))
+  if best.children:
+    chosen_state = state.clone()
+    chosen_state.apply_action(best.action)
+    print("Children of chosen:")
+    print(best.children_str(chosen_state))
+
+  return best.action
+
 
 if __name__ == '__main__':
   main()

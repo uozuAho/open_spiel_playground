06149daca2ea5815481359af9db6fbe3e966e975
Warwick <warwick_stone@fastmail.com>
2021-10-26 21:31:44 +1100

    client requests do_action

--- zmq_ttt/random_client.py
+++ zmq_ttt/random_client.py
@@ -1,4 +1,5 @@
 import json
+import random
 from typing import Dict
 
 import zmq
@@ -17,17 +18,29 @@ class ClientBot:
     self._socket.connect(server)
     # send any message to connect
     self._socket.send('hello'.encode('UTF-8'))
-    self._socket.recv().decode('UTF-8')
+    self._socket.recv()
 
   def run(self):
-    actions = self.get_legal_actions()
-    print(actions)
+    done = False
+    while not done:
+      actions = self.get_legal_actions()
+      if not actions:
+        done = True
+      else:
+        self.do_random_action(actions)
 
   def get_legal_actions(self) -> Dict:
     request = json.dumps({'type': 'legal_actions'})
     self._socket.send(request.encode('UTF-8'))
     raw_response = self._socket.recv().decode('UTF-8')
     return json.loads(raw_response)
+
+  def do_random_action(self, legal_actions: Dict):
+    # todo: actions can just be ints
+    action = random.choice(list(legal_actions.values()))
+    request = json.dumps({'type': 'do_action', 'action': action})
+    self._socket.send(request.encode('UTF-8'))
+    self._socket.recv()
 
 
 if __name__ == "__main__":

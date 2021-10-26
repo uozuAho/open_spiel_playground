995984dd45c50ed80233abc8b907a6a6d15a24fd
Warwick <warwick_stone@fastmail.com>
2021-10-26 19:14:18 +1100

    extract play_one_game

--- zmq_ttt/local_example.py
+++ zmq_ttt/local_example.py
@@ -6,12 +6,21 @@ from open_spiel.python.bots import uniform_random
 
 def main():
   game = pyspiel.load_game("tic_tac_toe")
-  state = game.new_initial_state()
   bot_1 = uniform_random.UniformRandomBot(0, np.random.RandomState())
   bot_2 = uniform_random.UniformRandomBot(1, np.random.RandomState())
 
-  players = [bot_1, bot_2]
+  state = play_one_game(game, bot_1, bot_2)
+
   player_labels = ['bot_1', 'bot_2']
+  winner = player_labels[0] if state.returns()[0] > 0 else player_labels[1]
+  print('final state:')
+  print(state)
+  print(f'winner: {winner}')
+
+
+def play_one_game(game, player_1, player_2):
+  players = [player_1, player_2]
+  state = game.new_initial_state()
 
   while not state.is_terminal():
     current_player_idx = state.current_player()
@@ -19,10 +28,7 @@ def main():
     action = current_player.step(state)
     state.apply_action(action)
 
-  winner = player_labels[0] if state.returns()[0] > 0 else player_labels[1]
-  print('final state:')
-  print(state)
-  print(f'winner: {winner}')
+  return state
 
 
 if __name__ == '__main__':

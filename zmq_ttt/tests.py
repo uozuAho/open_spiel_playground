cd8f15a898ab46b8268be3dff092b3ff678c78cd
Warwick <warwick_stone@fastmail.com>
2021-11-02 20:16:24 +1100

    test: mcts and random

--- zmq_ttt/tests.py
+++ zmq_ttt/tests.py
@@ -1,6 +1,5 @@
 import math
 from multiprocessing import Process
-import time
 import numpy as np
 from absl.testing import absltest
 
@@ -9,7 +8,6 @@ from open_spiel.python.bots import uniform_random
 
 from network_bot import NetworkBot, NetworkGame
 from game_server import TicTacToeServer
-from networking import DictClient
 
 
 class RemoteTicTacToeTests(absltest.TestCase):
@@ -30,23 +28,31 @@ class RemoteTicTacToeTests(absltest.TestCase):
 
   def test_mcts_vs_random_game(self):
     server = TicTacToeServer("tcp://*:5555")
-    server_process = Process(target=server.serve_one_game)
+    server_process = Process(target=server.run)
     server_process.start()
 
-    mcts_bot_builder = lambda game : mcts.MCTSBot(
+    game = NetworkGame(None, "tcp://localhost:5555")
+
+    mcts_bot = mcts.MCTSBot(
         game,
         uct_c=math.sqrt(2),
         max_simulations=2,
         evaluator=mcts.RandomRolloutEvaluator(n_rollouts=1))
-    bot = NetworkBot(mcts_bot_builder, "tcp://localhost:5555")
 
-    client_process = Process(target=bot.run)
-    client_process.start()
+    random_bot = uniform_random.UniformRandomBot(0, np.random.RandomState())
 
-    client_process.join()
+    players = [mcts_bot, random_bot]
+
+    state = game.new_initial_state()
+    while not state.is_terminal():
+      current_player = players[state.current_player()]
+      action = current_player.step(state)
+      state.apply_action(action)
+
+    game.exit()
     server_process.join()
-    # if we get here without hanging, success!
 
+  # todo: remove this
   def test_measure_performance(self):
     server = TicTacToeServer("tcp://*:5555")
     server_process = Process(target=server.measure_games_per_second, args=(0.5,))
@@ -67,12 +73,15 @@ class RemoteTicTacToeTests(absltest.TestCase):
     server_process = Process(target=server.run)
     server_process.start()
 
-    bot = uniform_random.UniformRandomBot(1, np.random.RandomState())
+    bot1 = uniform_random.UniformRandomBot(0, np.random.RandomState())
+    bot2 = uniform_random.UniformRandomBot(0, np.random.RandomState())
+    players = [bot1, bot2]
 
     game = NetworkGame(None, "tcp://localhost:5555")
     state = game.new_initial_state()
     while not state.is_terminal():
-      action = bot.step(state)
+      current_player = players[state.current_player()]
+      action = current_player.step(state)
       state.apply_action(action)
 
     game.exit()

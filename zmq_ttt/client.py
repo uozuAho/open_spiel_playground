23346ff5a52c39379cfc4615527ac42dddeca18f
Warwick <warwick_stone@fastmail.com>
2021-11-20 20:21:46 +1100

    zmq: add mcts to simple client

--- zmq_ttt/client.py
+++ zmq_ttt/client.py
@@ -1,16 +1,24 @@
+import math
+
 import numpy as np
 from open_spiel.python.bots import uniform_random
+from open_spiel.python.algorithms import mcts
 
 from network_game import NetworkGame
 
 
 def main():
-  bot1 = uniform_random.UniformRandomBot(0, np.random.RandomState())
   game = NetworkGame("tcp://localhost:5555")
+  # bot = uniform_random.UniformRandomBot(0, np.random.RandomState())
+  bot = mcts.MCTSBot(
+      game,
+      uct_c=math.sqrt(2),
+      max_simulations=3,
+      evaluator=mcts.RandomRolloutEvaluator(n_rollouts=2))
   state = game.new_initial_state()
 
   while not state.is_terminal():
-    action = bot1.step(state)
+    action = bot.step(state)
     print('bot action:', action)
     state.apply_action(action)
 
